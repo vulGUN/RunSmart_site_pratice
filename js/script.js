3e05de80f0007a36f6e9723860faf61327cc7e69
Max <exampel@gmail.com>
2022-02-01 21:03:25 +0300

    544554h

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// $(document).ready(function () {
+//   $('.carousel__inner').slick({
+//     speed: 900,
+//     adaptiveHeight: true,
+//     prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/left-arrow.svg"></button>',
+//     nextArrow: '<button type="button" class="slick-next"><img src="img/slider/right-arrow.svg"></button>',
+//     responsive: [
+//       {
+//         breakpoint: 992,
+//         settings: {
+//           dots: true,
+//           arrows: false,
+//         },
+//       },
+//     ],
+//   })
+// })
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  autoHeight: true,
+  speed: 900,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  responsive: {
+    768: {
+      nav: false,
+    },
+    0: {
+      nav: true,
+      navPosition: 'bottom',
+    },
+  },
+})
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev')
+})
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next')
+})
